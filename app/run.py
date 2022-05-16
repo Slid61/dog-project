21c77ef7af03ea72f4db1c4d800291b7be7068a1
Felipe Vasquez <f.vasquez.tavera@gmail.com>
2022-05-16 13:47:22 -0500

    Initial Commit

---
+++ app/run.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from extract_bottleneck_features import *
+from keras.preprocessing import image
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras.models import Sequential                  
+from tqdm import tqdm
+
+import os
+from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+bottleneck_features = np.load('bottleneck_features/DogVGG19Data.npz')
+train_VGG19 = bottleneck_features['train']
+valid_VGG19 = bottleneck_features['valid']
+test_VGG19 = bottleneck_features['test']
+
+VGG19_model = Sequential()
+VGG19_model.add(GlobalAveragePooling2D(input_shape=train_VGG16.shape[1:]))
+VGG19_model.add(Dense(133, activation='softmax'))
+
+VGG19_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+VGG19_model.load_weights('saved_models/weights.best.VGG19.hdf5')
+
+def VGG19_predict_breed(img_path):
+    # extract bottleneck features
+    bottleneck_feature = extract_VGG19(path_to_tensor(img_path))
+    # obtain predicted vector
+    predicted_vector = VGG19_model.predict(bottleneck_feature)
+    # return dog breed that is predicted by the model
+    return dog_names[np.argmax(predicted_vector)]
+
+
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file chosen')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded')
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    
+def VGG19_algo(img_path):
+    filename = os.path.basename(img_path)
+    breed = VGG19_predict_breed(img_path)
+    if (face_detector(img_path) == True):
+        print("Hello, human!")
+        display_image(filename)
+        print("You look like a...")
+        print(breed)
+    else:
+        print("What a cute dog!")
+        display_image(filename)
+        print("I think this is a...")
+        print(breed)
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
